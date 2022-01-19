b5cb052d931a6bd1873f262c0122b15a2121f6e8
emmanuel owusu <302559845@student.rocmondriaan.nl>
2022-01-19 07:47:07 +0100

    klaar

--- sd20-zoso-302559845-master/js/main.js
+++ sd20-zoso-302559845-master/js/main.js
@@ -1,4 +1,12 @@
- document.querySelector(".toggle-button").addEventListener
- ('click',() => {
-     document.body.classList.toggle("dark")
-})
+const divColour = document.querySelector(".colourchanging");
+const switchbutton = document.querySelector(".button");
+
+// switchbutton.addEventListener("click", changeColor);
+console.log(switchbutton);
+// function changeColor() {
+//     if (color.classList.contains("colourchanging")) {
+//         color.classList.add("color-change")
+//     }else{
+//         color.classList.remove("colour-change");
+//     }
+// }
